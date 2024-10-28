3b519ffb8e42cb3b7984663e294f19e0a5860455
Simpl1fy <gourabdas07012004@gmail.com>
2024-10-28 05:40:11 +0530

    added model files for mongodb collection

---
+++ texty-backend/database/Models/message.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }, 
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    timeStamp: {
+        type: Date,
+        default: Date.now,
+        required: true
+    }
+});
+
+const Message = mongoose.Model('Message', messageSchema);
+
+module.exports = Message;
