894a32a2d35b600a9fe299be729ae7416dbaeccf
Simpl1fy <gourabdas07012004@gmail.com>
2024-11-02 15:49:33 +0530

    added icons folder

--- texty-frontend/src/components/Signup.jsx
+++ texty-frontend/src/components/Signup.jsx
@@ -8,30 +8,12 @@ import {
 import { useState } from "react";
 import axios from 'axios';
 import { useAuth } from "../context/useAuth";
+import { ExcalamtionIcon } from '../icons/ExclamationIcon';
 
 
 const isValidEmail = (email) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
-}
-
-function Icon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={2}
-      stroke="currentColor"
-      className="h-6 w-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-      />
-    </svg>
-  )
 }
 
 export default function SimpleRegistrationForm() {
@@ -105,7 +87,7 @@ export default function SimpleRegistrationForm() {
             Nice to meet you! Enter your details to register.
         </Typography>
         {error && 
-          <Alert variant="gradient" color="red" icon={<Icon/>}>
+          <Alert variant="gradient" color="red" icon={<ExcalamtionIcon />}>
             {errorMessage}
           </Alert>
         }
