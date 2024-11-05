706d25f428b694e502e16aa896e7129222dfde11
Simpl1fy <gourabdas07012004@gmail.com>
2024-11-05 22:59:59 +0530

    added input in add contacts modal and removed condition of modal closing when clicked in dialog

--- texty-frontend/src/components/AddContactModal.jsx
+++ texty-frontend/src/components/AddContactModal.jsx
@@ -3,17 +3,45 @@ import {
   DialogHeader,
   DialogBody,
   DialogFooter,
+  Typography,
+  Input,
   Button,
 } from '@material-tailwind/react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
   
 export default function AddContactModal({ isModalOpen, toggleModal }) {
+
+  const [searchEmail, setSearchEmail] = useState('');
+
+  const handleEmailChange = (e) => {
+    let text = e.target.value;
+    setSearchEmail(text);
+    console.log(searchEmail);
+  }
+
   return (
     <>
-      <Dialog open={isModalOpen} handler={toggleModal}>
+      <Dialog open={isModalOpen}>
         <DialogHeader>Add a Contact</DialogHeader>
-        <DialogBody>Please search the contacts</DialogBody>
+        <DialogBody>
+          <Typography
+            variant='small'
+            color='blue-gray'
+            className="mb-1 font-medium"
+          >
+            Enter Email Address
+          </Typography>
+          <Input
+            placeholder='johnbrandy@gmail.com'
+            className="appearance-none !border-t-blue-gray-200 placeholder:text-blue-gray-300 placeholder:opacity-100 focus:!border-t-gray-900 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+            onChange={handleEmailChange}
+          />
+        </DialogBody>
         <DialogFooter>
           <Button
             variant='text'
