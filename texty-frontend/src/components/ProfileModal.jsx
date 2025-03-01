6bf8d391b06493519c7d6e94afe7bdec5ce3f64a
Simpl1fy <gourabdas07012004@gmail.com>
2025-03-01 11:12:42 +0530

    added profile picture to profile modal, and updated design of data

--- texty-frontend/src/components/ProfileModal.jsx
+++ texty-frontend/src/components/ProfileModal.jsx
@@ -7,6 +7,8 @@ import {
   DialogBody,
   DialogFooter,
 } from "@material-tailwind/react";
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import axios from "axios";
 import { useAuth } from "../context/useAuth";
 
@@ -32,20 +34,44 @@ export default function ProfileModal({ open, handleModal }) {
       }
     }
     if(isLoggedIn) {
+      console.log("Profile data will be fetched!");
       fetchProfileData();
     }
   }, []);
+
+  useEffect(() => {
+    console.log("Profile Data: ", profileData);
+  }, [profileData]);
 
   return (
     <>
       <Dialog open={open} handler={handleModal}>
         <DialogHeader>Profile</DialogHeader>
-        <DialogBody className="text-lg text-black ">
-          <div>
-            <span className="font-semibold">Name</span> - {profileData.name}
+        <DialogBody className="w-full px-3 mx-auto sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl xxl:max-w-xxl">
+          {/* Profile Picture */}
+          <div className="flex justify-center mb-[2.5rem]">
+            <img
+              src={profileData.profilePictureURL}
+              className="size-40 rounded-full object-cover"
+            />
           </div>
-          <div>
-            <span className="font-semibold">Email</span> - {profileData.email}
+          <div className="text-black flex flex-row items-center mb-[1rem]">
+            <div className="mr-[1.75rem]">
+              <PersonOutlineIcon />
+            </div>
+            <div className="flex flex-col">
+              <span className="font-bold">Name</span>
+              <span className="text-stone-700">{profileData.name}</span>
+            </div>
+          </div>
+          <div className="text-black flex flex-row items-center mb-[1rem]">
+            <div className="mr-[1.75rem]">
+              <MailOutlineIcon />
+            </div>
+            <div className="flex flex-col">
+              <span className="font-bold">Email</span>
+              <span className="text-stone-700">{profileData.email}</span>
+            </div>
           </div>
         </DialogBody>
         <DialogFooter>
