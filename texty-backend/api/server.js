2bdb895d9cccf2d45720252dc0a69da54928cc75
Simpl1fy <gourabdas07012004@gmail.com>
2025-03-19 19:57:41 +0530

    changes

--- texty-backend/api/server.js
+++ texty-backend/api/server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const http = require('http');
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const db = require('../database/db');
-const cors = require('cors');
-const setUpWebSocket = require('../websocket/webSocketConnection');
+const express = require("express");
+const http = require("http");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const db = require("../database/db");
+const cors = require("cors");
+const setUpWebSocket = require("../websocket/webSocketConnection");
 
 const app = express();
 
 const corsOptions = {
-    "origin": "http://localhost:3000",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-}
+  origin: "http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
 // cors middleware
 app.use(cors(corsOptions));
@@ -21,21 +21,27 @@ app.use(cors(corsOptions));
 // body parser
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  try {
+    return res
+      .status(200)
+      .send("Text Chat Application using react and express using Mongodb");
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
-app.get('/', (req, res) => {
-    res.status(200).send('Text Chat Application using react and express using Mongodb')
-})
+const userRoutes = require("../routes/userRoutes");
+app.use("/user", userRoutes);
 
-const userRoutes = require('../routes/userRoutes');
-app.use('/user', userRoutes);
-
-const chatRoutes = require('../routes/chatRoutes');
-app.use('/chat', chatRoutes);
+const chatRoutes = require("../routes/chatRoutes");
+app.use("/chat", chatRoutes);
 
 const server = http.createServer(app);
 setUpWebSocket(server);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`)
-})
+  console.log(`Listening on PORT ${PORT}`);
+});
